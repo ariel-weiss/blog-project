8f7de8460b2a6e6f1189e351b871894b26ab3e2f
Ariel Weiss <relweiss111@gmail.com>
2021-03-09 12:21:01 +0200

    Preparing for OAuth feature

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ const App = () => {
     return (
         <Container maxwidth='lg'>
             <AppBar className={classes.appBar} position='static' color='inherit'>
-                <Typography className={classes.heading} variant='h2' align='center'>My Blog</Typography>
+                <Typography className={classes.heading} variant='h2' align='center'>Tell Your Story</Typography>
                 <img className={classes.image} src={blog_image} alt="blog image" width='100'  />
             </AppBar>
             <Grow in>
