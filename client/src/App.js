b4675f5b55816a02b896b2629e2065c45ad3a668
Ariel Weiss <relweiss111@gmail.com>
2021-03-09 13:54:10 +0200

    Prepared all components needed for Auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,26 @@
-import React, { useEffect,useState } from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import { getPosts } from './actions/posts';
-import Form from './components/Form/Form';
-import Posts from './components/Posts/Posts';
-import blog_image from './images/blog_image.png';
+
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
 import useStyles from './styles'
+
 const App = () => {
-    const [currentId, setCurrentId] = useState(null);
     const classes = useStyles();
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [currentId,dispatch]);
-
 
     return (
-        <Container maxwidth='lg'>
-            <AppBar className={classes.appBar} position='static' color='inherit'>
-                <Typography className={classes.heading} variant='h2' align='center'>Tell Your Story</Typography>
-                <img className={classes.image} src={blog_image} alt="blog image" width='100'  />
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid container justify='space-between' alignItems='stretch' spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId} />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId}/>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-
-        </Container>
+        <BrowserRouter>
+            <Container maxwidth='lg'>
+                <Navbar />
+                <Switch>
+                    <Route path='/' exact component={Home} />
+                    <Route path='/auth' exact component={Auth} />
+                </Switch>
+            </Container>
+        </BrowserRouter>
     )
 }
 
